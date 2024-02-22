0354e105292dffa9c382fa80d27a773a3651cc0b
Tw0ur <twour@inbox.ru>
2024-02-22 23:47:38 +0600

    update urls , create template base.html

--- main/views.py
+++ main/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['1', '2']
-
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return render(request, 'main/about.html')
+    context = {
+        'title': "Home - О нас",
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему этот магазин такой классный, и какой хороший товар'
+    }
+    return render(request, 'main/about.html', context)
